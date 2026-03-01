e46d811812562908d38ce42a09b9068d655c6bb8
Sofia Rodriguez <sofia-isabel.rodriguez-cura@epitech.eu>
2026-03-02 00:39:49 +0100

    ADD: advanced 4040 header

--- include/advanced_components/Component4040.hpp
+++ include/advanced_components/Component4040.hpp
@@ -6,26 +6,24 @@
 */
 
 #ifndef INCLUDED_COMPONENT4040_HPP
-    #define INCLUDED_COMPONENT4040_HPP
+#define INCLUDED_COMPONENT4040_HPP
 
 #include "AComponent.hpp"
-#include <cstddef>
+#include <cstdint>
 
 namespace nts {
 
+class Component4040 : public virtual AComponent {
+private:
+    uint16_t _counter = 0;
+    Tristate _lastClock = False;
 
-
-class Component4040 : public virtual AComponent
-{
-    private:
-        size_t counter = 0;
-        nts::Tristate last_clock = nts::Tristate::False;
-    protected:
-    public:
-        Component4040();
-        void simulate(size_t tick) override; //we need to overwrite simulate since we need to update the flipflops even if they aren't called by another component, since they need to update themselves on the rising edge of the clock
-        nts :: Tristate compute ( std :: size_t pin ) override;
-
+public:
+    Component4040();
+    void simulate(size_t tick) override;
+    Tristate compute(size_t pin) override;
 };
+
 }
+
 #endif
